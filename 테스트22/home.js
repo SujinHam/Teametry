a782c0429253c80b56e3459a64068a92dd6624be
kaisea0830 <lhs020830@gmail.com>
2025-05-18 06:01:13 +0900

    Add files via upload

---
+++ 테스트22/home.js
@@ -0,0 +1,43 @@
+let mode = "";
+
+function goCreate() {
+  window.location.href = "create.html";
+}
+
+function startListFlow() {
+  mode = "list";
+  document.getElementById("room-code-box").classList.remove("hidden");
+  document.getElementById("password-box").classList.add("hidden");
+}
+
+function startJoinFlow() {
+  mode = "join";
+  document.getElementById("room-code-box").classList.remove("hidden");
+  document.getElementById("password-box").classList.add("hidden");
+}
+
+function submitRoomCode() {
+  const code = document.getElementById("roomCodeInput").value.trim();
+  if (!code) {
+    alert("방 코드를 입력하세요.");
+    return;
+  }
+
+  if (mode === "join") {
+    window.location.href = "join.html?code=" + encodeURIComponent(code);
+  } else if (mode === "list") {
+    localStorage.setItem("roomCode", code);
+    document.getElementById("room-code-box").classList.add("hidden");
+    document.getElementById("password-box").classList.remove("hidden");
+  }
+}
+
+function goToResult() {
+  const pw = document.getElementById("passwordInput").value.trim();
+  const code = localStorage.getItem("roomCode");
+  if (!pw) {
+    alert("비밀번호를 입력하세요.");
+    return;
+  }
+  window.location.href = `status.html?code=${encodeURIComponent(code)}&pw=${encodeURIComponent(pw)}`;
+}
